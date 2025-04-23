5049ec6cd23bcae7272b7dcd01c021410ca7fa32
Andres-AcerDev <andresitodoncel@gmail.com>
2025-04-23 11:34:15 -0500

    Agregar componente MovieList para mostrar una lista de películas

---
+++ src/components/MovieList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="movie-list">
+    <div class="row">
+      <div v-for="movie in movies" :key="movie.id" class="col-sm-6 col-md-4 mb-4">
+        <MovieCard :movie="movie" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { Movie } from '@/interfaces/Movie'
+import MovieCard from '@/components/MovieCard.vue'
+
+defineProps<{
+  movies: Movie[]
+}>()
+</script>
