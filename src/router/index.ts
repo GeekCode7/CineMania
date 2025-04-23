ae9dc4c1333915751fa52bf6ed3c4e18e6da70ad
Andres-AcerDev <andresitodoncel@gmail.com>
2025-04-23 11:28:48 -0500

    Refactor router configuration: simplificar definición de rutas y eliminar la vista About

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/movie/:id',
+    name: 'movie',
+    component: () => import('../views/MovieView.vue'),
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router
