64065cc096c32eefca1fd7cfed2eab23c91b741a
Andres-AcerDev <andresitodoncel@gmail.com>
2025-05-15 22:39:53 -0500

    Reorganiza la importación de estilos y ajusta la configuración de Vite para SCSS

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'bootstrap'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// Importar estilos principales (incluye Bootstrap)
+import '@/assets/styles/main.scss'
+
+// Importar Bootstrap Icons
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+// Importar JS de Bootstrap para componentes interactivos
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
